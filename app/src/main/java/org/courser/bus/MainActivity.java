321aa12e7b64d6ca5e04b532917215678ba6b752
Courser <GitCourser@users.noreply.github.com>
2025-01-31 22:10:31 +0800

    Add files via upload

---
+++ app/src/main/java/org/courser/bus/MainActivity.java
@@ -0,0 +1,30 @@
+package org.courser.bus;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://web.chelaile.net.cn/ch5/index.html?src=webapp_meizu_map");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
